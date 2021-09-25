2b764c2a2d192dc50ba11a297ad37fc232e43c9c
xrayd <72351211+xrayd@users.noreply.github.com>
2021-09-25 16:09:08 -0400

    feat: created BloomFilter shell class

---
+++ src/main/java/edu/brown/cs/student/main/BloomFilter.java
@@ -0,0 +1,36 @@
+package edu.brown.cs.student.main;
+
+public class BloomFilter {
+
+  /**
+   * BloomFilter constructor, called via the "users" command in REPL
+   */
+  BloomFilter(/** add constructor information here */){
+    //TODO: write constructor, create BloomFilter
+  }
+
+  /**
+   * Finds k similar users to a user with a certain id; called via the "similar" command in REPL
+   * @param k number of similar users to find
+   * @param userID user ID of user to find similar users to
+   */
+  public void similar(int k, int userID){
+    //TODO: write class
+  }
+
+  /**
+   * Also finds k similar users, but with the input user data instead of an ID
+   * Data types in constructor match that of the users database
+   * @param k number of users to find
+   * @param weight
+   * @param bust
+   * @param height
+   * @param age
+   * @param body
+   * @param horoscope
+   */
+  public void similar(int k, String weight, String bust, String height, int age,
+                      String body, String horoscope){
+    //TODO: write class
+  }
+}
